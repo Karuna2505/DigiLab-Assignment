a00a9a09384a63cf6527f8eeaf26a3ff7e5bc064
Karuna2505 <karunamathur14@gmail.com>
2024-04-06 22:17:47 +0530

    First Commit

---
+++ src/app/page.js
@@ -0,0 +1,55 @@
+"use client"
+import React, { useEffect,useState } from 'react';
+import toast from 'react-hot-toast';
+function ErrorMessage() {
+  return (
+    <div className="flex flex-col justify-center items-center w-[327px] min-h-[71px]">
+      <h1 className="text-[29px] font-medium leading-[31.9px] mb-4">
+        Lorem Ipsum...
+      </h1>
+      <h1 className="text-[19px] font-normal leading-[22.99px] text-white text-opacity-60">
+        Lorem ipsum dolor sit amet.
+      </h1>
+    </div>
+  );
+}
+
+function ImageContainer({isclicked}) {
+  return (
+    <div className="relative">
+      <img className={`object-cover w-full h-full ${isclicked ? 'shake' : ''}`} src="/fourth.svg" alt="Outer-Image"/>
+      <img className="absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2" src="/bell.svg" alt="inner-Image"/>
+    </div>
+  );
+}
+
+function Button({isclicked,setClicked}) {
+  const handleNotification = () =>{
+    setClicked(!isclicked)
+    toast("notification send")
+  }
+  return (
+    <button onClick={handleNotification} className="w-[327px] h-[47px] p-2 button-bg shadow-button font-semibold border-gradient gap-2">
+      Send Notification
+    </button>
+  );
+}
+
+export default function Home() {
+  const [isclicked,setClicked]=useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setClicked(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [isclicked]); 
+
+  return (
+    <div className="flex flex-col justify-evenly items-center h-screen">
+      <ErrorMessage />
+      <ImageContainer isclicked={isclicked}/>
+      <Button isclicked={isclicked} setClicked={setClicked}/>
+    </div>
+  );
+}
